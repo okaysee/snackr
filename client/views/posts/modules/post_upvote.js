45358222105fe2d8d66b5acf89720161a44157f1
Paul Asjes <p.asjes@gmail.com>
2015-01-29 17:42:12 -0500

    Implemented a downvote and removed the unvote functionality

--- client/views/posts/modules/post_upvote.js
+++ client/views/posts/modules/post_upvote.js
@@ -3,6 +3,11 @@ Template[getTemplate('postUpvote')].helpers({
     var user = Meteor.user();
     if(!user) return false;
     return _.include(this.upvoters, user._id);
+  },
+  downvoted: function() {
+    var user = Meteor.user();
+    if (!user) return false;
+    return _.include(this.downvoters, user._id);
   }
 });
 
@@ -15,16 +20,45 @@ Template[getTemplate('postUpvote')].events({
       flashMessage(i18n.t("please_log_in_first"), "info");
     }
     var upvoted = _.include(post.upvoters, Meteor.user()._id);
-    console.log(upvoted);
+    var downvoted = _.include(post.downvoters, Meteor.user()._id);
+
+    if (upvoted) return;
 
     if (!upvoted) {
       Meteor.call('upvotePost', post, function(error, result){
         trackEvent("post upvoted", {'_id': post._id});
       });
-    } else {
+    }
+
+    if (downvoted) {
+      Meteor.call('cancelDownvotePost', post, function(error, result) {
+        trackEvent('post downvote cancelled', {'_id': post._id});
+      });
+    }
+
+  },
+  'click .downvote-link': function(e, instance) {
+    var post = this;
+    e.preventDefault();
+
+    if (!Meteor.user()) {
+      Router.go('atSignIn');
+      flashMessage(i18n.t("please_log_in_first"), "info");
+    }
+
+    var upvoted = _.include(post.upvoters, Meteor.user()._id);
+    var downvoted = _.include(post.downvoters, Meteor.user()._id);
+
+    if (downvoted) return;
+
+    if (upvoted) {
       Meteor.call('cancelUpvotePost', post, function(error, result) {
         trackEvent('post upvote cancelled', {'_id': post._id});
       });
     }
+
+    Meteor.call('downvotePost', post, function(error, result) {
+        trackEvent('post downvoted', {'_id': post._id});
+      });
   }
 });
