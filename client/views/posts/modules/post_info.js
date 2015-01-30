307349e0bfb432654a1043823b92597c59859ad6
Paul Asjes <p.asjes@gmail.com>
2015-01-30 13:10:48 -0500

    changed points to noms

--- client/views/posts/modules/post_info.js
+++ client/views/posts/modules/post_info.js
@@ -1,6 +1,6 @@
 Template[getTemplate('postInfo')].helpers({
   pointsUnitDisplayText: function(){
-    return this.upvotes == 1 ? i18n.t('point') : i18n.t('points');
+    return this.upvotes == 1 ? 'nom' : 'noms';
   },
   getTemplate: function() {
     return getTemplate("postAuthor");
